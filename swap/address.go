c098fca95c284febf532c75677daa9a1d7482b91
oncecsc <oncecsc@gmail.com>
2021-03-30 17:33:44 +0800

    add erc20 abi

--- swap/address.go
+++ swap/address.go
@@ -41,8 +41,8 @@ func GetCreate2Address(factory, token0, token1 string) (pairAddress common.Addre
 }
 
 func sortAddressess(tkn0, tkn1 common.Address) (common.Address, common.Address) {
-	token0Rep := big.NewInt(0).SetBytes(tkn0.Bytes())
-	token1Rep := big.NewInt(0).SetBytes(tkn1.Bytes())
+	token0Rep := new(big.Int).SetBytes(tkn0.Bytes())
+	token1Rep := new(big.Int).SetBytes(tkn1.Bytes())
 
 	if token0Rep.Cmp(token1Rep) > 0 {
 		tkn0, tkn1 = tkn1, tkn0
