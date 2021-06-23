c256216a7e8407f06f3a87569ce1c30bac6078ef
oncecsc <oncecsc@gmail.com>
2021-06-23 17:08:34 +0800

    feat: convert x96 format to decimal.Decimal in uniswap v3

---
+++ swap/sqrt_price_x96_test.go
@@ -0,0 +1,16 @@
+package swap
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSqrtPX96ToPrice(t *testing.T) {
+	// weth/dai
+	wethPrice, _ := new(big.Int).SetString("1575977176149746651316824132", 10)
+
+	price := SqrtPriceX96ToPrice(wethPrice, true)
+	assert.Equal(t, "0.00039567688472264843148441298129", price.String())
+}
